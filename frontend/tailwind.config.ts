29a14d98b3b2e2445420ab281372612889cf69a4
SilkePilon <silkepilon2009@gmail.com>
2024-06-11 18:14:03 +0200

    refactor: Update opacity values in tailwind.config.ts for meteor animation

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -70,11 +70,11 @@ const config = {
           to: { height: "0" },
         },
         meteor: {
-          "0%": { transform: "rotate(215deg) translateX(0)", opacity: 1 },
-          "70%": { opacity: 1 },
+          "0%": { transform: "rotate(215deg) translateX(0)", opacity: "1" },
+          "70%": { opacity: "1" },
           "100%": {
             transform: "rotate(215deg) translateX(-500px)",
-            opacity: 0,
+            opacity: "0",
           },
         },
         grid: {
