863a0f8faee074e359803dcea55b94375ce48b00
SilkePilon <silkepilon2009@gmail.com>
2024-06-15 17:45:39 +0200

    refactor: Update CSS to hide horizontal scrollbar and improve image display consistency

--- frontend/src/components/nav.tsx
+++ frontend/src/components/nav.tsx
@@ -41,7 +41,7 @@ export function Nav({ links, isCollapsed }: NavProps) {
                       "dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white"
                   )}
                 >
-                  <link.icon className="h-4 w-4" />
+                  <link.icon className="h-6 w-6" />
                   <span className="sr-only">{link.title}</span>
                 </Link>
               </TooltipTrigger>
@@ -65,7 +65,7 @@ export function Nav({ links, isCollapsed }: NavProps) {
                 "justify-start"
               )}
             >
-              <link.icon className="mr-2 h-4 w-4" />
+              <link.icon className="mr-2 h-5 w-5" />
               {link.title}
               {link.label && (
                 <span
